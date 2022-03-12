f78dbe51f673f02998aa19830312f1c112714803
Matt Venn <matt@mattvenn.net>
2022-03-12 17:08:42 +0100

    first commit

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -82,40 +82,27 @@ module user_project_wrapper #(
 /* User project is instantiated  here   */
 /*--------------------------------------*/
 
-user_proj_example mprj (
+rgb_mixer rgb_mixed (
 `ifdef USE_POWER_PINS
 	.vccd1(vccd1),	// User area 1 1.8V power
 	.vssd1(vssd1),	// User area 1 digital ground
 `endif
 
-    .wb_clk_i(wb_clk_i),
-    .wb_rst_i(wb_rst_i),
+    .clk(wb_clk_i),
+    .reset(wb_rst_i),
 
-    // MGMT SoC Wishbone Slave
+    .enc0_a(io_in[ 8]),
+    .enc0_b(io_in[ 9]),
+    .enc1_a(io_in[10]),
+    .enc1_b(io_in[11]),
+    .enc2_a(io_in[12]),
+    .enc2_b(io_in[13]),
+    .pwm0_out(io_out[14]),
+    .pwm1_out(io_out[15]),
+    .pwm2_out(io_out[16]),
 
-    .wbs_cyc_i(wbs_cyc_i),
-    .wbs_stb_i(wbs_stb_i),
-    .wbs_we_i(wbs_we_i),
-    .wbs_sel_i(wbs_sel_i),
-    .wbs_adr_i(wbs_adr_i),
-    .wbs_dat_i(wbs_dat_i),
-    .wbs_ack_o(wbs_ack_o),
-    .wbs_dat_o(wbs_dat_o),
+    .io_oeb(io_oeb)
 
-    // Logic Analyzer
-
-    .la_data_in(la_data_in),
-    .la_data_out(la_data_out),
-    .la_oenb (la_oenb),
-
-    // IO Pads
-
-    .io_in (io_in),
-    .io_out(io_out),
-    .io_oeb(io_oeb),
-
-    // IRQ
-    .irq(user_irq)
 );
 
 endmodule	// user_project_wrapper
